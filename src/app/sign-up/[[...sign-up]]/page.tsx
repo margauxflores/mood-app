1c6ed68afed22843085003659ceb1f40b31f6f29
Margaux Flores <margaux@tailor.tech>
2023-09-22 23:21:59 +0900

    feat: implement clerk auth

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SignUp } from '@clerk/nextjs';
+
+const SignUpPage = () => {
+  return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />;
+};
+
+export default SignUpPage;
