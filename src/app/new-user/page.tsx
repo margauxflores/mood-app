d98b0544ea019bfa19234b5656cc799c1919e949
Margaux Flores <margaux@tailor.tech>
2023-09-23 15:02:42 +0900

    feat: add create new user record

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { currentUser } from '@clerk/nextjs';
+import { prisma } from '@/utils/db';
+import { redirect } from 'next/navigation';
 
-const NewUserPage = () => {
-  return <div>New User</div>;
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user?.id as string },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
+
+const NewUserPage = async () => {
+  await createNewUser();
+
+  return <div>...loading</div>;
 };
 
 export default NewUserPage;
